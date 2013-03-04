884d78f9147f1298b692af89700b0280eb14d4dd
Emiliano Barrionuevo <emiliano@orisom.com>
2013-03-04 01:08:17 -0300

    Pag69 : Agregado Unit Test CRUD contra la DB

---
+++ protected/tests/unit/ProjectTest.php
@@ -0,0 +1,42 @@
+<?php
+class ProjectTest extends CDbTestCase
+{
+	public function testCRUD()
+	{
+		//Create a new project
+		$newProject = new Project;
+		$newProjectNane = 'Test Project 1';
+		$newProject->setAttributes(
+			array(
+					'name'           => $newProjectNane,
+					'description'    => 'Test project number one',
+					'create_time'    => '2013-03-04 00:00:00',
+					'create_user_id' => 1,
+					'update_time'    => '2013-03-04 00:00:00',
+					'update_user_id' => 1,
+				)
+			);
+		$this->assertTrue($newProject->save(false));
+
+		//Read back the newly created project
+		$retrievedProject = Project::model()->findByPk($newProject->id);
+		$this->assertTrue($retrievedProject instanceof Project);
+		$this->assertEquals($newProjectNane, $retrievedProject->name);
+
+		//Update the newly created project
+		$updatedProjectName = 'Updated Test Project 1';
+		$newProject->name = $updatedProjectName;
+		$this->assertTrue($newProject->save(false));
+
+		//Read back the record again to ensure the update worked
+		$updatedProject = Project::model()->findByPk($newProject->id);
+		$this->assertTrue($updatedProject instanceof Project);
+		$this->assertEquals($updatedProjectName,$updatedProject->name);
+
+		//Delete the project
+		$newProjectID = $newProject->id;
+		$this->assertTrue($newProject->delete());
+		$deletedProject = Project::model()->findByPk($newProjectID);
+		$this->assertEquals(NULL, $deletedProject);
+	}
+}
