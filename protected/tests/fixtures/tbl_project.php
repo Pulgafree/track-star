60412f33d232f2e5432e3f4a0826586ff42cd40e
Emiliano Barrionuevo <emiliano@orisom.com>
2013-03-04 01:43:55 -0300

    Bug: Error en las keys de Fixture tbl_project

--- protected/tests/fixtures/tbl_project.php
+++ protected/tests/fixtures/tbl_project.php
@@ -9,7 +9,7 @@ return array(
 		'update_time' => '',
 		'update_user_id' => '',
 		),
-	'projec2t' => array(
+	'project2' => array(
 		'name' => 'Test Project 2',
 		'description' => 'This is test project 2',
 		'create_time' => '',
@@ -17,9 +17,9 @@ return array(
 		'update_time' => '',
 		'update_user_id' => '',
 		),
-	'project2' => array(
-		'name' => 'Test Project 2',
-		'description' => 'This is test project 2',
+	'project3' => array(
+		'name' => 'Test Project 3',
+		'description' => 'This is test project 3',
 		'create_time' => '',
 		'create_user_id' => '',
 		'update_time' => '',
